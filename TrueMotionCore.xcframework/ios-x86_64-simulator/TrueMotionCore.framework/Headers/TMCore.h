b008a09ce45f475f3e801ff7be73a46b81f408b7
Adam Horvath <adam.horvath@gotruemotion.com>
2020-04-27 22:04:44 +0200

    Update xcframework

---
+++ TrueMotionCore.xcframework/ios-x86_64-simulator/TrueMotionCore.framework/Headers/TMCore.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for TMCore.
+FOUNDATION_EXPORT double TMCoreVersionNumber;
+
+//! Project version string for TMCore.
+FOUNDATION_EXPORT const unsigned char TMCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Core/PublicHeader.h>
+
+
